7c733be8b667312c2a16ef766ae67e4616764e79
Uttam sharma <uttamftspl@gmail.com>
2025-04-21 01:28:00 +0530

    bcrypt js update

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 const userModel = require('../model/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const secret_key = '55354trw4fvrg65grtv56'
 
